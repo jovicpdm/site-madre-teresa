986bec33be06770403d59286e19660dbd0bc6f2f
João Victor Morais <45773455+jovicpdm@users.noreply.github.com>
2023-01-03 11:47:20 +0000

    header color change

--- src/components/customLink.js
+++ src/components/customLink.js
@@ -4,7 +4,7 @@ import Pallete from '../styles/pallete';
 
 const CustomLink = props => {
   return (
-    <Link href={props.href} color={Pallete.secondary} fontSize="1rem">
+    <Link href={props.href} color={Pallete.primaryLight} fontSize="1rem">
       {props.children}
     </Link>
   );
--- src/sections/header.js
+++ src/sections/header.js
@@ -10,27 +10,31 @@ import {
 } from '@chakra-ui/react';
 import Pallete from '../styles/pallete';
 import CustomLink from '../components/customLink';
+import {googleDriveView} from '../utils/consts';
 
-const Header = () => {
+const Header = props => {
   return (
     <SimpleGrid
       columns={[1, 2]}
-      bg={Pallete.primaryLight}
+      bg={Pallete.primaryDark}
       padding={['12px', '24px']}
+      // bgImage={googleDriveView + "1YvZ-KS7x1RsDlhmgnGxapceWu2GmtAzH"}
     >
       <Box display="flex" mt={2} alignItems="center">
         <Img
           marginRight={8}
-          height={[16,24]}
+          height={[16, 24]}
           borderRadius="full"
           src="https://jnkpsgcgjfgothmxgbue.supabase.co/storage/v1/object/sign/imt/Instagram%20post%20-%201.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbXQvSW5zdGFncmFtIHBvc3QgLSAxLnBuZyIsInRyYW5zZm9ybWF0aW9ucyI6IiIsImlhdCI6MTY3MjE2NzAzOSwiZXhwIjoxOTg3NTI3MDM5fQ.2ZIxUzzE9FPYWgwEKjtjcg0Stz1tagWu9MbBVrP_Nvo"
         />
         <Box textAlign={['center', 'left']} flexDirection="row">
-          <Heading fontSize={["20px", "36px"]} color="#3CACA5">Instituto Madre Teresa</Heading>
+          <Heading fontSize={['20px', '36px']} color="#F7F8F8">
+            Instituto Madre Teresa
+          </Heading>
           <Text
             fontSize={['12px', '20px']}
             fontWeight={'medium'}
-            color={Pallete.primary}
+            color="#f7f7f7"
           >
             Um novo conceito em gestão de saúde
           </Text>
